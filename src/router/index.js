3750ee2594b9a8a595421bf29c81ee58e175074e
Darkalex595 <darkalex595@gmail.com>
2021-10-07 21:39:03 -0600

    Se migraron los html a vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/item',
+      name: 'Item',
+      component: () => import('@/components/Item.vue')
+    },
+    {
+      path: '/url',
+      name: 'URL',
+      component: () => import('@/components/url.vue')
     }
   ]
 })
